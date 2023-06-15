56b260be757f3e07cabead453664134b841fd43f
mariareisfrancisco <maria.reis@studentfr.ch>
2023-06-15 11:11:42 +0200

    Correction of the nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
 var button = document.querySelector(".burger");
 var menu = document.querySelector(".menu");
+
 var openMenu = function () {
   menu.classList.toggle("is-open");
   button.classList.toggle("is-active");
 };
 
+var closeMenu = function () {
+  menu.classList.remove("is-open");
+  button.classList.remove("is-active");
+};
+
 button.addEventListener("click", openMenu);
+
+var menuLinks = menu.getElementsByTagName("a");
+
+for (var i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener("click", closeMenu);
+}
